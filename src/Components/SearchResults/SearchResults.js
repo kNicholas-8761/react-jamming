a96f09f0b82749017e79553423c6b389790a49b5
user <kamauri84@gmail.com>
2021-03-23 15:24:43 +1100

    create SearchResults folder with files

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import './SearchResult.css'
+
+const SearchResults = () =>{
+    return
+        <div className="SearchResults">
+         <h2>Results</h2>
+         <Tracklist />
+    </div>
+    )
+}
+
+export default SearchResults;
