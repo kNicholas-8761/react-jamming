1562b40e24e12898fc8225370681479d8f5a95f6
user <kamauri84@gmail.com>
2021-03-24 08:48:21 +1100

    add a constructor function to the App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,28 @@
-import React, {useState} from 'react';
+import React  from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults'
 import Playlist from '../Playlist/Playlist'
 
-const App = ()=> {
-  return(
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+       <h1>Ja<span className="highlight">mmm</span>ing</h1>
+       <div className="App">
+         <SearchBar />
+         <div className="App-playlist">
+           <SearchResults />
+           <Playlist />
 
-    </div>
-</div>
+      </div>
+  </div>
 </div>
   )
+  }
 }
 
 export default App;
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React from 'react'
 import './SearchResults.css'
 
 class SearchResults extends React.Component{
