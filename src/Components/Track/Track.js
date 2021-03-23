bd68181dad68ceebb59949266d1bdb4f7ba9b3ae
user <kamauri84@gmail.com>
2021-03-23 21:41:53 +1100

    create track folder with files

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Track.css';
+
+export const Track = () => {
+    return (
+        <div class="Track">
+            <div class="Track-information">
+                <h3></h3>
+                <p></p>
+       </div>
+        <button class="Track-action"></button>
+    </div>
+    )
+}
+export default Track;
