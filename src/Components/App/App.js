5107f68e27f58efd2b56de74a8764c09e9f6aa8c
user <kamauri84@gmail.com>
2021-03-24 11:34:25 +1100

    Add hard-coded values for playlistName and playlistTracks to state in App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,7 +11,12 @@ class App extends React.Component{
       SearchResults: [{name:'name1' , artist: 'artist1' , album: 'album1' , id: 1},
                       {name:'name2' , artist: 'artist2' , album: 'album2' , id: 2},
                       {name:'name3' , artist: 'artist3' , album: 'album3' , id: 3}
-    ]
+    ],
+    playlistName: 'My Playlist',
+    PlaylistTracks:[{name:'PlaylistName1' , artist: 'PlaylistArtist1' , album: 'PlaylistAlbum1' , id: 4},
+                    {name:'PlaylistName2' , artist: 'PlaylistArtist2' , album: 'PlaylistAlbum2' , id: 5},
+                    {name:'PlaylistName3' , artist: 'PlaylistArtist3' , album: 'PlaylistAlbum3' , id: 6},
+  ]
     }
   }
   render(){
