4e7372a8c7bacbddb52da90eddc25949ac0e3696
user <kamauri84@gmail.com>
2021-03-24 11:09:51 +1100

    use the .map() method to render each track in the tracks property

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './Tracklist.css';
+import Track from '../Track/Track';
 
-export const Tracklist = () => {
+class Tracklist extends React.Component{
+    render(){
     return (
         <div className="TrackList">
+            {
+                this.props.tracks.map(track =>{
+                    return <Track track={track}
+                                  key={track.id} />
+                })
+            }
         </div>
     )
+  }
 }
 
 export default Tracklist;
