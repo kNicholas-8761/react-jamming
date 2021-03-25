29ea2e11598b6f6c7d1faecb0ecc90a958ca5a5b
user <kamauri84@gmail.com>
2021-03-25 23:15:27 +1100

    create constant variables for application’s client ID and redirect URI

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,5 @@
+const clientId ='fa0df88c46604a4684ad017ddb6822fa';
+const redirectUri = 'http://localhost:3000/'
 let accessToken;
 
 const Spotify ={
@@ -6,8 +8,17 @@ const Spotify ={
             return accessToken;
         }
         //check for access token match
-        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
         const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            //This clears the parameters, aloowing us to grab a new access token when it expires.
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
     }
 }
 
