7afb4c9480fcd4bf61bee32b9d0649d8eb4d73f5
user <kamauri84@gmail.com>
2021-03-25 22:06:42 +1100

    check the URL to see if it has just been obtained

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -5,6 +5,9 @@ const Spotify ={
         if(accessToken){
             return accessToken;
         }
+        //check for access token match
+        const accessToken = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
     }
 }
 
