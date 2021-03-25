3ca1e5860e33b5778f350ef56f1cd34125ebd40a
user <kamauri84@gmail.com>
2021-03-25 14:40:51 +1100

    add an onChange attribute and set it equal to this.handleTermChange in the search bar's input

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -6,6 +6,9 @@ import './SearchBar.css';
 export class SearchBar extends React.Component{
     constructor(props){
         super(props)
+        this.state ={
+            term: ''
+        }
 
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
@@ -23,7 +26,7 @@ export class SearchBar extends React.Component{
 render(){
     return(
         <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
+            <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
             <button className="SearchButton">SEARCH</button>
     </div>
     )
